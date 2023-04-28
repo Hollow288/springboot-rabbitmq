a0e59c64d7b7bdf7ddb803fffc25943f431c889d
hollow <571339274@qq.com>
2023-04-28 17:02:50 +0800

    基础

---
+++ springboot-rabbitmq-consumers/src/main/java/com/hollow/SpringbootRabbitmqConsumersApplication.java
@@ -0,0 +1,13 @@
+package com.hollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqConsumersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitmqConsumersApplication.class, args);
+    }
+
+}
